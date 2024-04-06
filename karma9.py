3c1ce39b83b2b99415ea50c40172ab630d02b568
Sourabh Dey <146729006+CodeRreaper69@users.noreply.github.com>
2024-04-06 13:20:58 +0530

    Delete karma9.py

--- karma9.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-def main():
-    exe_path = os.path.join(os.path.dirname(__file__), 'karma9.exe')
-    subprocess.run([exe_path])
-
-if __name__ == '__main__':
-    main()
